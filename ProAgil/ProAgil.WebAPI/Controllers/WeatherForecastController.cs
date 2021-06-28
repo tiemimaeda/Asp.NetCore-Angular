dcd4946fd1f41962828762a71cfe1b347a0e8513
Priscila MAEDA <Priscila.MAEDA@consulting-for.accor.com>
2021-06-27 22:55:30 -0300

    projeto ProAgil Eventos - criação do projeto, model e conexão com o banco

---
+++ ProAgil/ProAgil.WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProAgil.WebAPI.Data;
+using ProAgil.WebAPI.Model;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgil.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        public readonly DataContext _context;
+        public WeatherForecastController(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {   
+            try
+            {
+                var results = await _context.Eventos.ToListAsync();
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+           try
+            {
+                var results = await _context.Eventos.FirstOrDefaultAsync(x => x.EventoId == id);
+                return Ok(results);
+            }
+            catch (System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Banco de Dados Falhou");
+            }
+        }
+           
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Curso Seja Full-Stack com Asp.NET Core, Angular + EF Core [2021]
