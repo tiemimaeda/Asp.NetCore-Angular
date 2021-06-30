b12438e423339841e540a0cdd9d652f701467aab
Priscila MAEDA <Priscila.MAEDA@consulting-for.accor.com>
2021-06-29 23:40:06 -0300

    inserindo tabela, imagens e binding

--- ProAgil/ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil/ProAgil-App/src/app/eventos/eventos.component.ts
@@ -8,12 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  eventos: any;
+  eventos:  any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
   
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
   }
 
   getEventos() {
--- ProAgil/ProAgil.WebAPI/Startup.cs
+++ ProAgil/ProAgil.WebAPI/Startup.cs
@@ -46,6 +46,7 @@ namespace ProAgil.WebAPI
 
             app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseRouting();
+            app.UseStaticFiles();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
