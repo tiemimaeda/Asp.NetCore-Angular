bc5b7c6051790510f96e21810237b1d4f3b420d7
Priscila MAEDA <Priscila.MAEDA@consulting-for.accor.com>
2021-07-19 22:51:13 -0300

    observable

--- ProAgil/ProAgil-App/src/app/eventos/eventos.component.ts
+++ ProAgil/ProAgil-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Evento } from '../_models/Evento';
+import { EventoService } from '../_services/evento.service';
 
 @Component({
   selector: 'app-eventos',
@@ -17,19 +18,19 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
   }
 
-  eventosFiltrados: any = [];
-  eventos:  any = [];
+  eventosFiltrados!: Evento[];
+  eventos!: Evento[];
   imagemLargura = 50;
   imagemMargem = 2;
   mostrarImagem = false;
   
-  constructor(private http: HttpClient) { }
+  constructor(private eventoService: EventoService) { }
 
   ngOnInit() {
     this.getEventos();
   }
 
-  filtrarEventos(filtrarPor: string): any {
+  filtrarEventos(filtrarPor: string): Evento[] {
     filtrarPor = filtrarPor.toLocaleLowerCase();
     return this.eventos.filter(
       (      evento: { tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
@@ -41,8 +42,11 @@ export class EventosComponent implements OnInit {
   }
 
   getEventos() {
-    this.http.get('http://localhost:5000/weatherforecast').subscribe(response => {
-      this.eventos = response
+    this.eventoService.getAllEvento().subscribe(
+    (_eventos: Evento[]) => {
+      this.eventos = _eventos;
+      this.eventosFiltrados = this.eventos;
+      console.log(_eventos)
     }, error => {
       console.log(error)
     });
